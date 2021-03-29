1afd30bc8bf9c4de369eb17cd9f9b8d110805b1b
Ruslan Kalyniak <krv241085krv@icloud.com>
2021-03-29 22:22:10 +0300

    Updates

---
+++ precache-manifest.779d2be3da2dd62321570a07394e3199.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f165dacb55b0f561dee0a7b6541d1cdf",
+    "url": "./index.html"
+  },
+  {
+    "revision": "715084c7ae1f7fd47853",
+    "url": "./static/css/main.3149eb3f.chunk.css"
+  },
+  {
+    "revision": "a3fa78d2a47eeb043805",
+    "url": "./static/js/2.ac9e10eb.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.ac9e10eb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "715084c7ae1f7fd47853",
+    "url": "./static/js/main.9c76bc90.chunk.js"
+  },
+  {
+    "revision": "8b3c3b96386eee1ea33a",
+    "url": "./static/js/runtime-main.b672bc02.js"
+  }
+]);
